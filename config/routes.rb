af58258b3369ead99dd214d5059afaa803dd6ec6
JeffButler <butler.jeff1@gmail.com>
2013-12-29 13:25:51 -0800

    Added header base buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ZipTutoring::Application.routes.draw do
-  get "/main_tab", :controller => 'main_tab', :action => 'index'
+  get "/home", :controller => 'home', :action => 'index'
   get "/tutor", :controller => 'tutor', :action => 'index'
   get "/recommendation", :controller => 'recommendation', :action => 'index'
-  root :to => redirect('/main_tab')
+  post "/login", :controller => 'login', :action => 'index'
+  get "/login", :controller => 'login', :action => 'index'
+  root :to => redirect('/home')
 end
