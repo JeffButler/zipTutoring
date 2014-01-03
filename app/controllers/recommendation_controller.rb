d2faa444c0817a3f4c39e1cd0b38e5b35183ee6c
JeffButler <butler.jeff1@gmail.com>
2014-01-03 12:19:17 -0800

    Base functationality across board

--- app/controllers/recommendation_controller.rb
+++ app/controllers/recommendation_controller.rb
@@ -1,5 +1,7 @@
 class RecommendationController < ApplicationController
-def index 
-
-end 
+  def index 
+    if Recommendation.all.length > 0
+      @recommendations = Recommendation.all
+    end 
+  end 
 end
--- app/controllers/tutor_controller.rb
+++ app/controllers/tutor_controller.rb
@@ -1,6 +1,6 @@
 class TutorController < ApplicationController
   set_tab :tutor
  def index 
-
+   @tutors = TutorBio.all
  end
 end
