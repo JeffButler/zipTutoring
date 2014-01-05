ab6f9345a47d6084b1462f5dcea4b2f20f54ce0a
JeffButler <butler.jeff1@gmail.com>
2014-01-05 12:49:32 -0800

    CSS adjustmnets

--- app/models/tutor_bio.rb
+++ app/models/tutor_bio.rb
@@ -5,5 +5,6 @@ class TutorBio < ActiveRecord::Base
     :thumb=> "100x100#",
     :small  => "150x150>",
     :medium => "300x300>",
-    :large =>   "400x400>" }
+    :large =>   "400x400>" }, 
+    :s3_credentials => S3_CREDENTIALS
 end
