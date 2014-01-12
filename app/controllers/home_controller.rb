9b4d033a7c433a49d4660391b6af63bb95ec111c
JeffButler <butler.jeff1@gmail.com>
2014-01-12 15:45:58 -0800

    Making Progress with Home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
  
   def index 
+   if Recommendation.all.length > 0
+     length = Recommendation.all.length
+     r = Random.new
+     random = r.rand(0...length)
+     @rec = Recommendation.all[random]
+   end
    if AdminBio.all.length > 0
       @bio = AdminBio.all.first
     end
--- app/controllers/home_controller.rb~
+++ app/controllers/home_controller.rb~
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
  
   def index 
-   
+   if AdminBio.all.length > 0
+      @bio = AdminBio.all.first
+    end
   end
 end
